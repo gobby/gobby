05f026facbe317e26de827a0c531be67f0f761ae
Philipp Kern <phil@0x539.de>
2007-01-06 22:50:26 +0000

    [project @ Added GNU regex wrappers (not used yet because they suck at unicode)]
    Original author: Benjamin Herr <ben@0x539.de>
    Date: 2005-10-28 18:01:55+00:00

---
+++ src/regex.cpp
@@ -0,0 +1,131 @@
+#include <glib/gunicode.h>
+#include <regex.h>
+#include "regex.hpp"
+
+const regex::compile_options regex::compile_options::NONE
+	= regex::compile_options(0);
+const regex::compile_options regex::compile_options::EXTENDED
+	= regex::compile_options(REG_EXTENDED);
+const regex::compile_options regex::compile_options::IGNORE_CASE
+	= regex::compile_options(REG_ICASE);
+const regex::compile_options regex::compile_options::NO_OFFSETS
+	= regex::compile_options(REG_NOSUB);
+const regex::compile_options regex::compile_options::NEWLINE
+	= regex::compile_options(REG_NEWLINE);
+
+const regex::match_options regex::match_options::NONE
+	= regex::match_options(0);
+const regex::match_options regex::match_options::NOT_BOL
+	= regex::match_options(REG_NOTBOL);
+const regex::match_options regex::match_options::NOT_EOL
+	= regex::match_options(REG_NOTEOL);
+
+/* It would be fun if these were actually useful,
+ * but I really do not know what to do with them.
+const regex::compile_error regex::compile_error::BAD_BACKREF
+	= regex::compile_error(REG_BADBR);
+const regex::compile_error regex::compile_error::BAD_PATTERN
+	= regex::compile_error(REG_BADPAT);
+const regex::compile_error regex::compile_error::BAD_REPEAT
+	= regex::compile_error(REG_BADRPT);
+const regex::compile_error regex::compile_error::UNMATCHED_BRACE
+	= regex::compile_error(REG_EBRACE);
+const regex::compile_error regex::compile_error::UNMATCHED_BRACKET
+	= regex::compile_error(REG_EBRACKET);
+const regex::compile_error regex::compile_error::INVALID_COLLATION
+	= regex::compile_error(REG_ECOLLATE);
+const regex::compile_error regex::compile_error::BAD_CHARCLASS
+	= regex::compile_error(REG_ECTYPE);
+const regex::compile_error regex::compile_error::NON_SPECIFIC
+	= regex::compile_error(REG_EEND);
+const regex::compile_error regex::compile_error::TRAILING_BACKSLASH
+	= regex::compile_error(REG_EESCAPE);
+const regex::compile_error regex::compile_error::UNMATCHED_PAREN
+	= regex::compile_error(REG_EPAREN);
+const regex::compile_error regex::compile_error::INVALID_RANGE
+	= regex::compile_error(REG_ERANGE);
+const regex::compile_error regex::compile_error::BUFFER_TOO_BIG
+	= regex::compile_error(REG_ESIZE);
+const regex::compile_error regex::compile_error::OUT_OF_MEMORY
+	= regex::compile_error(REG_ESPACE);
+const regex::compile_error regex::compile_error::INVALID_SUBEXP_REF
+	= regex::compile_error(REG_ESUBREG);
+
+*/
+
+#define regex_ static_cast<regex_t*>(m_regex)
+
+regex::compile_error::compile_error(void* regex, int value)
+	: runtime_error(make_message(regex, value)) {}
+
+regex::compile_error::~compile_error() throw() {}
+
+const char* regex::compile_error::make_message(void* pregex, int value) {
+	const regex_t * const regex = static_cast<regex_t*>(pregex);
+	m_value = value;
+	std::vector<char> errbuf(regerror(value, regex, 0, 0));
+	regerror(value, regex, &errbuf[0], errbuf.size());
+	m_message = &errbuf[0];
+
+	return m_message.c_str();
+}
+
+regex::regex(const char* regex_string, compile_options cflags) {
+	m_regex = new regex_t;
+	int errcode = regcomp(regex_, regex_string, cflags.get_value());
+	if (errcode != 0) {
+		this->~regex();
+		throw compile_error(m_regex, errcode);
+	}
+}
+
+regex::~regex() {
+	regfree(regex_);
+	delete regex_;
+}
+
+void regex::reset(const char* regex_string, compile_options cflags) {
+	regfree(regex_);
+	int errcode = regcomp(regex_, regex_string, cflags.get_value());
+	if (errcode != 0) {
+		throw compile_error(m_regex, errcode);
+	}
+}
+
+bool regex::match(const char* string, match_options eflags) {
+	return regexec(regex_, string, 0, 0, eflags) == 0;
+}
+
+bool regex::find(const char* string,
+                 match_positions matches,
+                 match_options eflags) {
+	std::vector<regmatch_t> pmatch(matches.size());
+	if (regexec(regex_, string, matches.size(), &pmatch[0], eflags) != 0)
+		return false;
+
+	std::vector<regmatch_t>::iterator j = pmatch.begin();
+	for (match_positions::iterator i = matches.begin();
+	     i != matches.end();
+	     ++i, ++j) {
+		i->first =
+			g_utf8_pointer_to_offset(string, string + j->rm_so);
+		i->second =
+			g_utf8_pointer_to_offset(string, string + j->rm_eo);
+	}
+
+	return true;
+}
+
+bool regex::find(const char* string, std::pair<size_t, size_t>& matchpos,
+          regex::match_options eflags) {
+	regmatch_t pmatch;
+	if (regexec(regex_, string, 1, &pmatch, eflags) != 0)
+		return false;
+
+	matchpos.first =
+		g_utf8_pointer_to_offset(string, string + pmatch.rm_so);
+	matchpos.second =
+		g_utf8_pointer_to_offset(string, string + pmatch.rm_eo);
+
+	return true;
+}
