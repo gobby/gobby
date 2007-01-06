9a188d39ebb742fdaf01591bbe7c210e43eb7824
Philipp Kern <phil@0x539.de>
2007-01-06 22:27:45 +0000

    [project @ Autotoolised gobby]
    Original author: Philipp Kern <phil@0x539.de>
    Date: 2005-03-30 17:56:03+00:00

--- Makefile
+++
@@ -1,2 +0,0 @@
-gobby:
-	g++ src/window.cpp src/main.cpp -o gobby -Iinc `pkg-config --cflags --libs gtkmm-2.4 lobby-1.0`
---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/usr/bin/env aclocal
+/usr/bin/env autoheader
+/usr/bin/env autoconf
+/usr/bin/env automake
+
