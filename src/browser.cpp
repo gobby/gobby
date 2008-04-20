4215a907f533b91f3cd05f00f8a8b2066ec6fdf9
Armin Burgmeier <armin@arbur.net>
2008-04-20 18:48:48 +0200

    Fixed a format string
    2008-04-20  Armin Burgmeier  <armin@0x539.de>
    
    	* src/browser.cpp: Fixed a format string (%1 instead of %0).

--- src/browser.cpp
+++ src/browser.cpp
@@ -195,7 +195,7 @@ void Gobby::Browser::on_hostname_activate()
 	StatusBar::MessageHandle message_handle =
 		m_status_bar.add_message(
 			StatusBar::INFO, Glib::ustring::compose(
-				_("Resolving %0…"), host), 0);
+				_("Resolving %1…"), host), 0);
 
 	m_resolv_map[resolv_handle].message_handle = message_handle;
 }
