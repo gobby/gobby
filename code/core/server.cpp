2379bae61c303270f2dd84fbd27c7d8f12db6df1
Armin Burgmeier <armin@arbur.net>
2014-01-15 10:11:28 +0100

    Add the Gobby::Server class
    This class will be used by the self-hosting functionality to run a TCP
    server which accepts incoming connections.

---
+++ code/core/server.cpp
@@ -0,0 +1,248 @@
+/* Gobby - GTK-based collaborative text editor
+ * Copyright (C) 2008-2014 Armin Burgmeier <armin@arbur.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include "core/selfhoster.hpp"
+#include "util/file.hpp"
+#include "util/i18n.hpp"
+
+#include <libinfinity/common/inf-cert-util.h>
+#include <libinfinity/common/inf-error.h>
+
+#include <gnutls/x509.h>
+
+#include <cassert>
+
+Gobby::Server::Server(InfIo* io,
+                      InfLocalPublisher* publisher):
+	m_io(io), m_publisher(publisher),
+	m_xmpp4(NULL), m_xmpp6(NULL), m_pool(NULL)
+{
+	g_object_ref(m_io);
+}
+
+Gobby::Server::~Server()
+{
+	if(is_open()) close();
+	set_pool(NULL);
+
+	g_object_unref(m_io);
+}
+
+void Gobby::Server::open(unsigned int port,
+                         InfXmppConnectionSecurityPolicy security_policy,
+                         InfCertificateCredentials* creds)
+{
+	// If we can open one of tcp4 or tcp6 that's a success.
+	InfdTcpServer* tcp4;
+	InfdTcpServer* tcp6;
+
+	if(is_open() && get_port() == port)
+	{
+		set_credentials(security_policy, creds);
+		return;
+	}
+
+	static const guint8 ANY6_ADDR[16] =
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	InfIpAddress* any6 = inf_ip_address_new_raw6(ANY6_ADDR);
+
+	tcp4 = INFD_TCP_SERVER(g_object_new(
+		INFD_TYPE_TCP_SERVER,
+		"io", m_io, "local-address", NULL,
+		"local-port", port,
+		NULL));
+	tcp6 = INFD_TCP_SERVER(g_object_new(
+		INFD_TYPE_TCP_SERVER,
+		"io", m_io, "local-address", any6,
+		"local-port", port,
+		NULL));
+	inf_ip_address_free(any6);
+
+	if(!infd_tcp_server_open(tcp6, NULL))
+	{
+		g_object_unref(tcp6);
+		tcp6 = NULL;
+
+		GError* error = NULL;
+		if(!infd_tcp_server_open(tcp4, &error))
+		{
+			g_object_unref(tcp4);
+
+			const std::string message = error->message;
+			g_error_free(error);
+
+			throw std::runtime_error(message);
+		}
+	}
+	else
+	{
+		if(!infd_tcp_server_open(tcp4, NULL))
+		{
+			g_object_unref(tcp4);
+			tcp4 = NULL;
+		}
+	}
+
+	// We have the new server open, from this point on there is nothing
+	// that can go wrong anymore. Therefore, close the old server and
+	// take over the new one.
+	if(is_open()) close();
+
+	InfXmppConnectionSecurityPolicy policy =
+		INF_XMPP_CONNECTION_SECURITY_ONLY_UNSECURED;
+	if(creds != NULL) policy = security_policy;
+
+	if(tcp4)
+	{
+		m_xmpp4 = infd_xmpp_server_new(
+			tcp4, security_policy, creds, NULL, NULL);
+		g_object_unref(tcp4);
+	}
+
+	if(tcp6)
+	{
+		m_xmpp6 = infd_xmpp_server_new(
+			tcp6, security_policy, creds, NULL, NULL);
+		g_object_unref(tcp6);
+	}
+
+	if(m_pool)
+	{
+		if(m_xmpp4 != NULL)
+		{
+			infd_server_pool_add_server(
+				m_pool, INFD_XML_SERVER(m_xmpp4));
+			infd_server_pool_add_local_publisher(
+				m_pool, m_xmpp4, m_publisher);
+		}
+
+		if(m_xmpp6 != NULL)
+		{
+			infd_server_pool_add_server(
+				m_pool, INFD_XML_SERVER(m_xmpp6));
+			infd_server_pool_add_local_publisher(
+				m_pool, m_xmpp6, m_publisher);
+		}
+	}
+}
+
+void Gobby::Server::close()
+{
+	g_assert(is_open());
+
+	if(m_xmpp6 != NULL)
+	{
+		// Will be removed from server pool automatically
+		infd_xml_server_close(INFD_XML_SERVER(m_xmpp6));
+		g_object_unref(m_xmpp6);
+		m_xmpp6 = NULL;
+	}
+
+	if(m_xmpp4 != NULL)
+	{
+		// Will be removed from server pool automatically
+		infd_xml_server_close(INFD_XML_SERVER(m_xmpp4));
+		g_object_unref(m_xmpp4);
+		m_xmpp4 = NULL;
+	}
+}
+
+bool Gobby::Server::is_open() const
+{
+	return m_xmpp4 != NULL || m_xmpp6 != NULL;
+}
+
+unsigned int Gobby::Server::get_port() const
+{
+	g_assert(is_open());
+
+	InfdXmppServer* xmpp = m_xmpp6;
+	if(xmpp == NULL) xmpp = m_xmpp4;
+	g_assert(xmpp != NULL);
+
+	InfdTcpServer* tcp;
+	g_object_get(G_OBJECT(xmpp), "tcp-server", &tcp, NULL);
+
+	guint port;
+	g_object_get(G_OBJECT(tcp), "local-port", &port, NULL);
+	g_object_unref(tcp);
+
+	return port;
+}
+
+void Gobby::Server::set_credentials(InfXmppConnectionSecurityPolicy policy,
+                                    InfCertificateCredentials* credentials)
+{
+	if(m_xmpp6 != NULL)
+	{
+		g_object_set(
+			G_OBJECT(m_xmpp6),
+			"security-policy", &policy,
+			"credentials", credentials,
+			NULL);
+	}
+
+	if(m_xmpp4 != NULL)
+	{
+		g_object_set(
+			G_OBJECT(m_xmpp4),
+			"security-policy", &policy,
+			"credentials", credentials,
+			NULL);
+	}
+}
+
+void Gobby::Server::set_pool(InfdServerPool* pool)
+{
+	if(m_pool != NULL)
+	{
+		if(m_xmpp4 != NULL)
+			infd_server_pool_remove_server(
+				m_pool, INFD_XML_SERVER(m_xmpp4));
+		if(m_xmpp6 != NULL)
+			infd_server_pool_remove_server(
+				m_pool, INFD_XML_SERVER(m_xmpp6));
+
+		g_object_unref(m_pool);
+	}
+
+	m_pool = pool;
+
+	if(m_pool != NULL)
+	{
+		g_object_ref(m_pool);
+
+		if(m_xmpp4 != NULL)
+		{
+			infd_server_pool_add_server(
+				m_pool, INFD_XML_SERVER(m_xmpp4));
+			infd_server_pool_add_local_publisher(
+				m_pool, m_xmpp4, m_publisher);
+		}
+
+		if(m_xmpp6 != NULL)
+		{
+			infd_server_pool_add_server(
+				m_pool, INFD_XML_SERVER(m_xmpp6));
+			infd_server_pool_add_local_publisher(
+				m_pool, m_xmpp6, m_publisher);
+		}
+	}
+}
+
---
+++ code/core/server.hpp
@@ -0,0 +1,67 @@
+/* Gobby - GTK-based collaborative text editor
+ * Copyright (C) 2008-2014 Armin Burgmeier <armin@arbur.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef _GOBBY_SERVER_HPP_
+#define _GOBBY_SERVER_HPP_
+
+#include <libinfinity/server/infd-xmpp-server.h>
+#include <libinfinity/server/infd-server-pool.h>
+#include <libinfinity/common/inf-local-publisher.h>
+#include <libinfinity/common/inf-io.h>
+
+namespace Gobby
+{
+
+class Server
+{
+public:
+	Server(InfIo* io, InfLocalPublisher* publisher);
+	~Server();
+
+	// Can throw; can be used on already open servers:
+	void open(unsigned int port,
+	          InfXmppConnectionSecurityPolicy security_policy,
+	          InfCertificateCredentials* creds);
+
+	void close();
+
+	bool is_open() const;
+
+	unsigned int get_port() const;
+
+	// Use new credentials on running server, for new connections:
+	void set_credentials(InfXmppConnectionSecurityPolicy security_policy,
+	                     InfCertificateCredentials* credentials);
+
+	// Set a server pool to which to add the servers, and which to
+	// use for publishing with the publisher.
+	void set_pool(InfdServerPool* pool);
+
+protected:
+	InfIo* m_io;
+	InfLocalPublisher* m_publisher;
+
+	InfdXmppServer* m_xmpp4;
+	InfdXmppServer* m_xmpp6;
+	InfdServerPool* m_pool;
+};
+
+}
+	
+#endif // _GOBBY_SERVER_HPP_
